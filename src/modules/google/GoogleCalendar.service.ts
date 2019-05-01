0cd22382b8ad06a44c39a2c1b51a8f075073ffa2
Dragoș Străinu <str.dr4605@gmail.com>
2019-05-01 23:24:51 +0300

    Delete unused function

--- src/modules/google/GoogleCalendar.service.ts
+++ src/modules/google/GoogleCalendar.service.ts
@@ -3,30 +3,6 @@ import { GoogleAPI } from './GoogleAPI.service';
 import { IAddEvent, ICalendar, ICreateCalendar, IEvent, IEventsList } from './interface';
 
 export class GoogleCalendarService {
-  /**
-   * Lists the next 10 events on the user's primary calendar.
-   * @param options - options
-   */
-  static listEvents(options) {
-    GoogleAPI.authorizeAndExec(auth => {
-      const calendar = google.calendar({ version: 'v3', auth });
-      calendar.events.list(options, (err, res) => {
-        // tslint:disable:no-console
-        if (err) throw new Error('The API returned an error: ' + err);
-        const events = res.data.items;
-        if (events.length) {
-          console.log('Upcoming 10 events:');
-          events.map((event, i) => {
-            const start = event.start.dateTime || event.start.date;
-            console.log(`${start} - ${event.summary}`);
-          });
-        } else {
-          console.log('No upcoming events found.');
-        }
-      });
-    });
-  }
-
   static async listCalendars(): Promise<any> {
     return await GoogleAPI.authorizeAndExec(auth => {
       const calendar = google.calendar({ version: 'v3', auth });
